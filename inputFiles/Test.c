c6eafe56efec8b76c3c68c3984e2989218baefde
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2022-03-18 17:08:32 +0100

    Handlingconst feature

--- inputFiles/Test.c
+++ inputFiles/Test.c
@@ -1,2 +1,7 @@
-float a = 1.1f;
-printf(a);
+/*
+* My program
+*/
+int x = 5*(3/10 + 9/10);
+float y = x*2/( 2+1 * 2/3 +x) +8 * (8/4);
+float result = x + y; //calculate the result
+printf(result); //show the result
